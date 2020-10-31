df5e68ebd22142880fe46b11668a74aa3eeb7a57
AdityaGattu <52188412+AdityaGattu@users.noreply.github.com>
2020-10-31 22:41:18 +0530

    Update Delete without head pointer.cpp

--- Delete without head pointer.cpp
+++ Delete without head pointer.cpp
@@ -100,6 +100,9 @@ struct Node {
 }*head;
 */
 
+// This function should delete node from linked list. The function
+// may assume that node exists in linked list and is not last node
+// node: reference to the node which is to be deleted
 void deleteNode(Node *node)
 {
    // Your code here
